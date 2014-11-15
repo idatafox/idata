34de3fdd167808d783c04245e3db57a1cf0ba44e
idatafox <wishlong@yahoo.com>
2014-11-15 22:43:31 +0800

    final commit

---
+++ elearn007-velocity-base-theme/docroot/WEB-INF/src/com/elearn007/velocity/test/ElearnUser.java
@@ -0,0 +1,21 @@
+package com.elearn007.velocity.test;
+
+public class ElearnUser {
+ String userName=null;
+ 
+ public ElearnUser()
+ {
+	 
+ }
+ 
+  public void setUserName(String userNameV)
+  {
+	  this.userName=userNameV;
+  }
+  
+  public String getUserName()
+  {
+	  return this.userName;
+  }
+ 
+}
